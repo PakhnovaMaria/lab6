2383f26f8d1e6bc183f7bd6838480b116119bb46
PakhnovaMaria <PakhnovaMaria@users.noreply.github.com>
2015-12-04 11:25:25 +0300

    Update A.py

--- A.py
+++ A.py
@@ -3,12 +3,12 @@ N = int(f.readline())
 A = f.readline().split()
 for i in range(len(A)):
     A[i] = int(A[i])
-    
+
 for i in range(len(A) - 1):
     for j in range(i + 1, len(A)):
         if A[i] == A[j]:
             x = A[i]
-            
-f = open('output. txt', 'w')
+
+f = open('output.txt', 'w')
 print(x, file = f)
 f.close()
