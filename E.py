6f17f004698aba2373f124116fa127e4be536afa
PakhnovaMaria <PakhnovaMaria@users.noreply.github.com>
2015-12-04 12:33:54 +0300

    Update E.py

--- E.py
+++ E.py
@@ -38,3 +38,25 @@
 | 3 1 100 |            |
 +---------+------------+
 '''
+
+
+
+f = open('input.txt')
+A = f.readline().split()
+N = int(A[0])
+K = int(A[1])
+S = [0] * N
+for i in range(K):
+    B = f.readline().split()
+    for j in range(len(B)):
+        B[j] = int(B[j])
+    for a in range(1,N+1):
+        if B[0] == a:
+            S[a-1] -= B[2]
+        if B[1] == a:
+            S[a-1] += B[2]
+
+f = open('output.txt', 'w')
+print(S, file = f)
+f.close()
+
